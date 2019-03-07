c641b1a523bcf6a606fe54b8bc7e70aafea2cfd2
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-07 15:21:44 -0700

    db setup

---
+++ data/migrations/20190307151510_bootstrap.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('supplies', function(tbl) {
+    tbl.increments();
+    tbl
+      .string('name', 128)
+      .notNullable()
+      .unique();
+    tbl.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExist('supplies');
+};
