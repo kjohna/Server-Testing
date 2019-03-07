894794025e497a8b15af5cc9996476f5cd6a9af9
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-07 15:01:34 -0700

    project setup, server and tests configured and working

---
+++ api/server.spec.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+
+const server = require('./server.js');
+
+describe('server.js', () => {
+  it('should set testing environment', () => {
+    expect(process.env.DB_ENV).toBe('testing');
+  });
+
+  describe('GET /', () => {
+    it('should return 200 OK', async () => {
+      const res = await request(server).get('/');
+      expect(res.status).toBe(200);
+    });
+  });
+});
