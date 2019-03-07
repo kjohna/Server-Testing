2f84a9ad73074e73ebd430c1b28ebc1a2a71033f
kjohna <43828763+kjohna@users.noreply.github.com>
2019-03-07 15:05:18 -0700

    fill out basic tests for GET to '/'

--- api/server.spec.js
+++ api/server.spec.js
@@ -6,11 +6,21 @@ describe('server.js', () => {
   it('should set testing environment', () => {
     expect(process.env.DB_ENV).toBe('testing');
   });
-
   describe('GET /', () => {
+    // test response code
     it('should return 200 OK', async () => {
       const res = await request(server).get('/');
       expect(res.status).toBe(200);
+    });
+    // test response data type
+    it('should return JSON', async () => {
+      const res = await request(server).get('/');
+      expect(res.type).toBe('application/json');
+    });
+    // test shape of response body
+    it('should return { server: "works! }', async () => {
+      const res = await request(server).get('/');
+      expect(res.body).toEqual({ server: 'works!' });
     });
   });
 });
